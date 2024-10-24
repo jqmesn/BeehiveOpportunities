4c3fc57cc21e56f335e5d7bbcf545a82d61ff938
Catalystiq <73408095+Catalystiq@users.noreply.github.com>
2024-10-24 09:34:41 -0600

    added Prisma to project
    Prisma ORM added in order to read from a mongoDB database

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
 	import '../app.css';
 </script>
 
-<div class=" font-[475] m-0">
-    <div class="bg-white h-8 flex justify-between items-center h-30 px-8 py-8">
+<div class=" font-normal m-0">
+    <div class="bg-white flex justify-between items-center h-[30px] px-8 py-8">
         <div class="no-underline text-[#fe9b20] text-3xl"><a href="/">Beehive Opportunities</a></div>
     
         <div class="flex text-xl justify-between">
@@ -22,16 +22,14 @@
         </div>
     </div>
 
-    <div class="w-full">
-        <div class="w-full h-[300px] bg-[#fe9b20] flex justify-center items-center mb-[10px]">
-            <p class="text-[80px] m-0 text-white text-center">Beehive Opportunities</p>
-        </div>
-    </div>
-    
-    <div class="bg-slate-600 text-white p-30 flex justify-center items-center text-center absolute bottom-0 w-full h-18">
-        This blog is student ran and all content is produced by students
-    </div>
-    
+
+
     <slot></slot>
+    
+    <div class="bg-slate-600 text-white p-30 flex justify-center items-center text-center absolute bottom-0 w-full h-[7.5%]">
+        <p>This blog is student ran and all content is produced by students</p>
+    </div>
+    
+
 </div>
 
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="w-full">
+    <div class="w-full h-[300px] bg-[#fe9b20] flex justify-center items-center mb-[10px]">
+        <p class="text-[80px] m-0 text-white text-center">Beehive Opportunities</p>
+    </div>
+</div>
